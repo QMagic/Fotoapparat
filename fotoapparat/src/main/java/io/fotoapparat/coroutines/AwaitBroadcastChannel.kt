b7f548089a191649b48ef79f9b67ae1f471e6a4d
Quentin Sallat <quentin@sensopia.com>
2019-06-28 17:07:53 -0400

    [Android] Migrating to stable coroutines

--- fotoapparat/src/main/java/io/fotoapparat/coroutines/AwaitBroadcastChannel.kt
+++ fotoapparat/src/main/java/io/fotoapparat/coroutines/AwaitBroadcastChannel.kt
@@ -1,16 +1,17 @@
 package io.fotoapparat.coroutines
 
-import kotlinx.coroutines.experimental.CompletableDeferred
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.channels.BroadcastChannel
-import kotlinx.coroutines.experimental.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 
 /**
  * A [ConflatedBroadcastChannel] which exposes a [getValue] which will [await] for at least one value.
  */
 internal class AwaitBroadcastChannel<T>(
-        private val channel: ConflatedBroadcastChannel<T> = ConflatedBroadcastChannel(),
-        private val deferred: CompletableDeferred<Boolean> = CompletableDeferred()
+    private val channel: ConflatedBroadcastChannel<T> = ConflatedBroadcastChannel(),
+    private val deferred: CompletableDeferred<Boolean> = CompletableDeferred()
 ) : BroadcastChannel<T> by channel, Deferred<Boolean> by deferred {
 
     /**
@@ -32,6 +33,15 @@ internal class AwaitBroadcastChannel<T>(
     }
 
     override fun cancel(cause: Throwable?): Boolean {
-        return channel.cancel(cause) && deferred.cancel(cause)
+        if (cause is CancellationException) {
+            channel.cancel(cause)
+            deferred.cancel(cause)
+        }
+        return true
+    }
+
+    override fun cancel(cause: CancellationException?) {
+        channel.cancel(cause)
+        deferred.cancel(cause)
     }
 }
